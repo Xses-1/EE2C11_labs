184b754d1cbfc1f60642356f0e8d8ef6acd839f8
Wiktor Tomanek <wiktort52@gmail.com>
2023-09-20 11:39:49 +0200

    Added the while loop, averaging, and changed the points to somwhere in the middle

--- VT_approx.py
+++ VT_approx.py
@@ -14,18 +14,30 @@ Vgs = []
 #slope of last two coordinates
 #y2-y1/x2-x1
 
-y1 = np.sqrt(Id[-1])
-y2 = np.sqrt(Id[-2])
+Vt = 0
+i = 0
+while i < 41: 
+    y1 = np.sqrt(Id[-46])   # Set the points to the 46th and 47th because there is 91
+    y2 = np.sqrt(Id[-47])   # points, so this is somwhere in the middle
 
-x1 = Vgs[-1]
-x2 = Vgs[-2]
+    x1 = Vgs[-46 - i*90]
+    x2 = Vgs[-47 - i*90]
 
-Slope = (y2 - y1) / (x2 - x1)
-# y = slope(x - x1) + y1
-# Vt = x when y = 0
+    Slope = (y2 - y1) / (x2 - x1)
+    # y = slope(x - x1) + y1
+    # Vt = x when y = 0
 
-y = 0
+    y = 0
 
-Vt = ( (y - y1)/Slope ) + x1
+    Vt = Vt + ( (y - y1)/Slope ) + x1 # This might cause an overflow, but whatever; fuck it
+                                      # Let's see wheather Python can take that;
+                                      # This is not my fault that I can't even check for fucking
+                                      # overflow because this piece of garbage languge is supposed to
+                                      # do fucking memory management for you;
+                                      # in C/C++ I know that long long is 64bit or whatever and won't
+                                      # overflow, but idk here; fuck this shit
 
+    i += 1
 
+Vt = Vt / 41
+print(Vt);
