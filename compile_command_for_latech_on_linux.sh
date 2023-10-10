f4ae9af7fad48f1ada854359ecc1554d56936bf1
Wiktor Tomanek <wiktort52@gmail.com>
2023-10-10 16:07:28 +0200

    Copied the template, wrote the introduction

--- compile_command_for_latech_on_linux.sh
+++ compile_command_for_latech_on_linux.sh
@@ -2,4 +2,4 @@
 
 # kill -15 $(pgrep firefox)
 : | pdflatex -halt-on-error *.tex | grep '^!.*' -A200 --color=always
-# firefox /home/xses/Desktop/Resume Wiktor Tomanek-1.pdf *.pdf &
+firefox *.pdf &
