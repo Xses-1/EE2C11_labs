28cca8ec92ea7503ff2afc47b8acf19c3fdb507d
vigo <mathieu.demeestere04@gmail.com>
2023-10-10 15:20:48 +0200

    Vt_approx now has a better file format and plots

--- VT_approx.py
+++ VT_approx.py
@@ -1,64 +1,57 @@
 import numpy as np
 import json
+import matplotlib.pyplot as plt
 from pprint import pprint
 
-
-in_file = open("sample.json")
-
-sample = json.loads(in_file.read())
-
-Id = []
-Vgs = []
-Vds = []
-
-#known variables
-for i in range(1, len(sample)-1):
-    Id.append([])
-    Vgs.append([])
-    Vds.append([])
-    for j in range(1, len(sample[i])-1):
-        Id[i-1].append(float(sample[i][j][1]))
-        Vgs[i-1].append(float(sample[i][j][0]))
+def FormatSample(sample):
+    #known variables
+    for i in range(0, len(sample)-1):
+        Id.append([])
+        Vgs.append([])
+        match sample[i][0]:
+                case "600m":
+                    Vds.append(0.6)
+                case "630m":
+                    Vds.append(0.63)
+                case "660m":
+                    Vds.append(0.66)
+                case "690m":
+                    Vds.append(0.69)
+                case "720m":
+                    Vds.append(0.72)
+                case "750m":
+                    Vds.append(0.75)
+                case "780m":
+                    Vds.append(0.78)
+                case "810m":
+                    Vds.append(0.81)
+                case "840m":
+                    Vds.append(0.84)
+                case "870m":
+                    Vds.append(0.87)
+                case "900m":
+                    Vds.append(0.90)
+                case "930m":
+                    Vds.append(0.93)
+                case "960m":
+                    Vds.append(0.96)
+                case "990m":
+                    Vds.append(0.99)
+                case _ :
+                    Vds.append(float(sample[i][0]))
+        for j in range(1, len(sample[i])-1):
+            Id[i-1].append(float(sample[i][j][1]))
+            Vgs[i-1].append(round(float(sample[i][j][0]),4))
 
             
-        match sample[i][0]:
-            case "600m":
-                Vds[i-1].append(0.6)
-            case "630m":
-                Vds[i-1].append(0.63)
-            case "660m":
-                Vds[i-1].append(0.66)
-            case "690m":
-                Vds[i-1].append(0.69)
-            case "720m":
-                Vds[i-1].append(0.72)
-            case "750m":
-                Vds[i-1].append(0.75)
-            case "780m":
-                Vds[i-1].append(0.78)
-            case "810m":
-                Vds[i-1].append(0.81)
-            case "840m":
-                Vds[i-1].append(0.84)
-            case "870m":
-                Vds[i-1].append(0.87)
-            case "900m":
-                Vds[i-1].append(0.90)
-            case "930m":
-                Vds[i-1].append(0.93)
-            case "960m":
-                Vds[i-1].append(0.96)
-            case "990m":
-                Vds[i-1].append(0.99)
-            case _ :
-                Vds[i-1].append(float(sample[i][0]))
+    print(len(Id[1]))
+    print("\n")
+    print(len(Vds))
+    print("\n")
+    print(len(Vgs[1]))
+    print("\n")
 
-#formula : Id = 1/2 k (Vgs - Vt) ^ 2
 
-#slope of y = sqrt(Id) x = Vgs in saturation region, calculate intersect on x axis -> Vt
-
-#slope of last two coordinates
-#y2-y1/x2-x1
 
 def linear_region(x,y):
     derivatives = []
@@ -67,68 +60,124 @@ def linear_region(x,y):
         x2 = x[i]
         y1 = y[i-1]
         y2 = y[i]
+        print(y1,y2,x1,x2)
         slope = (y2 - y1) / (x2 - x1)
+        
         derivatives.append(slope)
+    print('\n\n')
+
    
 
     maxder = derivatives.index(max(derivatives))
-    print
     return maxder
 
 
-Vt = []
-K = []
-K2 = []
-lamarray = []
-i = 1
-while i < len(Id): 
-    index = linear_region(Id[i], Vgs[i])
-    print (index)
-    y1 = np.sqrt(Id[i][50])   # Set the points to the 46th and 47th because there is 91
-    y2 = np.sqrt(Id[i][50 + 5])   # points, so this is somwhere in the middle
+def findVal():
+    Vt = []
+    K = []
+    K2 = []
+    lamarray = []
+    for i in range(1, len(Id)) :
 
-    x1 = Vgs[i][50]
-    x2 = Vgs[i][50 + 5]
+        ################################
+        #Find the Treshold Voltage
+        ################################
 
-    Slope = (y2 - y1) / (x2 - x1)
-    # y = slope(x - x1) + y1
-    # Vt = x when y = 0
-    # Vt = -y1
+        #Val VGs i val Vgs 25 because that is assumed to be in the saturation region 
 
-    Vt.append((-y1/Slope) + x1) 
+        #formula : Id = 1/2 k (Vgs - Vt) ^ 2
 
-    K.append((2*(Slope**2))/((x1-Vt[i-1])**2))
+        #slope of y = sqrt(Id) x = Vgs in saturation region, calculate intersect on x axis -> Vt
+        
+        #slope of last two coordinates
+        #y2-y1/x2-x1
 
-    #Finding Lamda line
-    x1 = Vds[i-1][50]
-    x2 = Vds[i][50]
+        y1 = np.sqrt(Id[i][25])   
+        y2 = np.sqrt(Id[i][25 + 5])  
 
-    y1 = Id[i-1][50]
-    y2 = Id[i][50]
+        x1 = Vgs[i][25]
+        x2 = Vgs[i][25 + 5]
 
-    slope = (y2 - y1) / (x2 - x1)
+        Slope = (y2 - y1) / (x2 - x1)
+        # y = slope(x - x1) + y1
+        # Vt = x when y = 0
+        # Vt = -y1/slope + x1
 
-    # lamda = -1/vds when id = 0
+        vt = -y1/slope + x1
+        Vt.append(vt) 
 
-    vdslam = (-y1/slope) + x1
+        #---------------------------------------------------------------- Vt Found
 
-    lamarray.append(-1/vdslam)
+        
 
-    #K2: Id = idprime when vds = 0 
+        K.append((2*(Slope**2))/((x1-Vt[i-1])**2))
 
-    idprime = (-slope * x1) + y1
+        #Finding Lamda line
+        x1 = Vds[i-1][50]
+        x2 = Vds[i][50]
 
-    K2.append((2*idprime)/((Vgs[i]-Vt[i-1])**2))
-     
+        y1 = Id[i-1][50]
+        y2 = Id[i][50]
 
-    i += 1
+        slope = (y2 - y1) / (x2 - x1)
 
-vt = np.average(Vt)
-k = np.average(K)
-k2 = np.average(K2)
-lamda = np.average(lamarray)
+        # lamda = -1/vds when id = 0
 
-print("vt = " + str(vt))
-print("k = " + str(k))
-print("k2 = " + str(k2))
-print("lamda = " + str(lamda))
+        vdslam = (-y1/slope) + x1
+
+        lamarray.append(-1/vdslam)
+
+        #K2: Id = idprime when vds = 0 
+
+        idprime = (-slope * x1) + y1
+
+        K2.append((2*idprime)/((Vgs[i]-Vt[i-1])**2))
+
+    pprint(K2)
+    vt = np.average(Vt)
+    k = np.average(K)
+    k2 = np.average(K2)
+    lamda = np.average(lamarray)
+
+    print("vt = " + str(vt))
+    print("k = " + str(k))
+    print("k2 = " + str(k2))
+    print("lamda = " + str(lamda))
+
+def plotSample(id, vgs, vds):
+    # place the legend, given the labels above for each of the lines.
+    for i in range(1, len(vds)-1):
+        #plot Id vs 
+        sqid = np.sqrt(id[i])
+        plt.plot(vgs[i], id[i])
+
+        
+    plt.legend(loc="upper left")
+
+    # Place labels at x- and y-axis, and title above the plot.
+    # Matplotlib can use LaTeX notation for formulas.
+    # Braces can be doubled {{}} to escape f-string interpolation
+    plt.xlabel(f'$V_{{DS}}~(V)$')
+    plt.ylabel(f'$I_{{D}}~(mA)$')
+    plt.title(f'$I_D$ as a function of $V_{{GS}}$ for different values of $V_{{DS}}$')
+    
+    plt.savefig("Id" + ".pdf")
+
+    # show it on the screen
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    in_file = open("EE2C11_labs/sample.json")
+
+    sample = json.loads(in_file.read())
+
+    Id = []
+    Vgs = []
+    Vds = []
+
+    FormatSample(sample)
+
+    plotSample(Id, Vgs, Vds)
+
